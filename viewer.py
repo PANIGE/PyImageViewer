ddcd6570f5c5f5cff3560e94901547ba0eba1798
PANIGE <Nicolas.alary@gmx.fr>
2021-12-08 16:31:25 +0100

    Initial Commit

---
+++ viewer.py
@@ -0,0 +1,46 @@
+import pip
+
+
+try:
+    __import__("PIL")
+except ImportError:
+    pip.main(['install', "Pillow"])   
+from PIL import Image
+import os
+import sys
+
+args = sys.argv
+
+char = "██"
+colored = True
+
+  
+
+
+def rgb(red, green, blue):
+  return f'\x1b[38;2;{red};{green};{blue}m'
+
+
+def printPic(file, size, char="██"):
+    print(rgb(255,255,255))
+    file = args[1]
+    file.strip("'")
+    file.strip('"')
+
+    img = Image.open(file)
+    img.load()
+    img.thumbnail((size, size))
+    pixels = list(img.getdata())
+    width, height = img.size
+    pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+    os.system("cls")
+    for y in pixels:
+        for x in y:
+            if colored:
+                print(rgb(x[0], x[1], x[2]) + char, end="")
+            else:
+                avg = int(sum((x[0], x[1], x[2])) / 3)
+                print(rgb(avg, avg, avg) + char, end="")
+        print(rgb(255,255,255))
+        
+printPic(args[1], int(args[2]))
